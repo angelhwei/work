b0e2384a0ed82eda516ea663f8758f69f45c5644
Angel <angelhwei12@gamil.com>
2025-02-18 19:07:54 +0800

    Delete personal_website folder

--- personal_website/app/src/app/components/index.ts
+++
@@ -1,9 +0,0 @@
-import { HeaderComponent } from './header/header.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ExperiencesSectionComponent } from './experiences-section/experiences-section.component';
-
-export const components = [HeaderComponent, NavbarComponent, ExperiencesSectionComponent];
-
-export * from './header/header.component';
-export * from './navbar/navbar.component';
-export * from './experiences-section/experiences-section.component';
--- personal_website/app/src/app/containers/index.ts
+++
@@ -1,5 +0,0 @@
-import { HeaderComponent } from "../components/";
-
-export const containers = [HeaderComponent];
-
-export * from "../components/";
